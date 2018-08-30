77dc02a43585aa0344fe9de8d0155550b94dfea6
bagus212 <bagusdewangga2@gmail.com>
2018-08-30 11:38:01 +0700

    home page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LineComponent } from './line/line.component';
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
 
 const routes: Routes = [{
   path:'',component:HomeComponent,children:[
     {path:'',redirectTo:'/line',pathMatch:'full'},
-    {path:'line',component:LineComponent}
+    {path:'line',component:LineComponent},
+    {path:'bar',component:BarComponent},
+    {path:'pie',component:PieComponent}
   ]
 }];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,11 +5,27 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { LineComponent } from './line/line.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule
+
   ],
-  declarations: [HomeComponent, LineComponent]
+  declarations: [HomeComponent, LineComponent, BarComponent, PieComponent]
 })
 export class HomeModule { }
